fc23289fcc9777bfd54103be2058f3f48b5eb2fb
Bartosz Iwiński <01bartosz0001@gmail.com>
2020-02-11 18:12:18 +0100

    Place image in the center

--- app/script.js
+++ app/script.js
@@ -66,12 +66,12 @@ class App extends React.Component {
     return (
       <div>
         <h1>Protect your eyes</h1>
-        <div className={this.state.status === 'off' ? 'showed' : 'hidden'}>
+        <div className={this.state.status === 'off' ? '' : 'hidden'}>
           <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
           <p>This app will help you track your time and inform you when it's time to rest.</p>
         </div>
-        <img src="./images/work.png" className={this.state.status === 'work' ? 'showed' : 'hidden'}/>
-        <img src="./images/rest.png" className={this.state.status === 'rest' ? 'showed' : 'hidden'}/>
+        <img src="./images/work.png" className={this.state.status === 'work' ? 'image' : 'hidden'}/>
+        <img src="./images/rest.png" className={this.state.status === 'rest' ? 'image' : 'hidden'}/>
         <div className={this.state.status != 'off' ? 'timer' : 'hidden'}>
           {this.formatTime()}
         </div>
